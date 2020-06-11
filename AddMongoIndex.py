eb39b790d409b65e71f885280b87277a5d45f0f9
yhl <yhliangedit@163.com>
2020-06-11 10:45:27 +0800

    添加创建索引记录

--- AddMongoIndex.py
+++ AddMongoIndex.py
@@ -1,12 +1,23 @@
 from dbClient.mongoClient import MongoClient
+from record.record import  Record
 
+# mongoConn = {
+#     'host': '10.0.1.186',
+#     'port':8635 ,
+#     'db': 'gpshisdata',
+#     'authDb': 'gpshisdata',
+#     'userName': 'gphis',
+#     'password': 'Openstack#2020'
+# }
+
+# local
 mongoConn = {
-    'host': '10.0.1.186',
-    'port':8635 ,
+    'host': '192.168.10.41',
+    'port': 27017,
     'db': 'gpshisdata',
-    'authDb': 'gpshisdata',
-    'userName': 'gphis',
-    'password': 'Openstack#2020'
+    'authDb': 'admin',
+    'userName': 'admin',
+    'password': 'openstack'
 }
 
 
@@ -14,13 +25,29 @@ mongoConn = {
 # 每次处理的数据量
 everyCount = 10000
 
+# 存储处理数据历史记录的文件地址
+recordPath = 'index.db'
 
 if __name__ == '__main__':
     mongoDb = MongoClient(mongoConn)
+    record = Record(recordPath)
+
+    name_list = []
+
+    print("1")
+    # 后期记录表中的记录
+    if record.findKeyExist("record"):
+        # 存在该记录, 取出处理的数据量
+        name_list.extend(record.getHandledNum("record"))
 
     for name in  mongoDb.getDocument("tb_vehicle_status_his"):
+        if name in name_list:
+            continue
         print("Create index: {}".format(name))
         mongoDb.addIndex(name)
+
+        name_list.append(name)
+        record.updateHandledNum("record", name_list)
 
 
 
